131c48c7cdac57fc0485ce08537b84c9128f5fb7
Askar K. Akshabaev <a.akshabaev@kbtu.kz>
2017-01-19 12:50:13 +0600

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Main function 
+        /// </summary>
+        /// <param name="args">arguments from console</param>
+        static void Main(string[] args)
+        {
+            // comment
+            /*
+             * comment 1
+             * comment 2
+             */
+        }
+    }
+}
